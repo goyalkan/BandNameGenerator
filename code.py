4c1936f08766bb6e4594d02b8fba837b32e07d26
Kanishka Goyal <127941423+goyalkan@users.noreply.github.com>
2023-08-02 20:38:56 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+
+a = input("What's the name of the city you grew up in?\n")
+
+b = input("What's your pet's name?\n")
+
+print("your band name could be " + a + " " + b)
